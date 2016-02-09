8312a0508b408f1ee9506e6c6add5adaaf5ed71d
Lukas Weibel <lukas.weibel@bluewin.ch>
2016-02-09 20:28:31 +0100

    Working button example.

--- src/main/java/ch/smaug/light/server/LightControl.java
+++ src/main/java/ch/smaug/light/server/LightControl.java
@@ -5,8 +5,12 @@ import org.slf4j.LoggerFactory;
 
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
 import com.pi4j.io.gpio.GpioPinPwmOutput;
+import com.pi4j.io.gpio.PinPullResistance;
 import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 import com.pi4j.wiringpi.Gpio;
 
 public class LightControl {
@@ -18,6 +22,8 @@ public class LightControl {
 
 	private final GpioPinPwmOutput out;
 
+	private final GpioPinDigitalInput myButton;
+
 	public static LightControl getLightControl() {
 		return INSTANCE;
 	}
@@ -26,8 +32,20 @@ public class LightControl {
 		final GpioController gpio = GpioFactory.getInstance();
 		out = gpio.provisionPwmOutputPin(RaspiPin.GPIO_01);
 		Gpio.pwmSetMode(Gpio.PWM_MODE_BAL);
-		Gpio.pwmSetRange(100);
-		Gpio.pwmSetClock(10);
+		Gpio.pwmSetRange(1000);
+		Gpio.pwmSetClock(100);
+
+		myButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_29, PinPullResistance.PULL_UP);
+		myButton.addListener(new GpioPinListenerDigital() {
+			@Override
+			public void handleGpioPinDigitalStateChangeEvent(final GpioPinDigitalStateChangeEvent event) {
+				LOG.info(event.getEventType().name() + " GPIO PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
+				if (event.getState().isLow()) {
+					setValue((getValue() + 100) % 1000);
+				}
+			}
+
+		});
 	}
 
 	public int getValue() {
@@ -35,8 +53,8 @@ public class LightControl {
 	}
 
 	public void setValue(final int value) {
-		if (value < 0 || value > 100) {
-			throw new IllegalArgumentException("Only value between 0 and 100 allowed.");
+		if (value < 0 || value > 1000) {
+			throw new IllegalArgumentException("Only value between 0 and 1000 allowed.");
 		}
 		LOG.info("Set value from {} to {}.", this.value, value);
 		this.value = value;
